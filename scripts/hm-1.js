cb2c95eab8534169533eee3ee5fd4503b6f80083
ElenaF95 <fachira.elena@gmail.com>
2023-09-07 19:37:58 +0300

    added hm-1.js file in scripts folder

---
+++ scripts/hm-1.js
@@ -0,0 +1,16 @@
+let variable = 'Hello World';
+if (variable === 'Hello World') alert('Hello World');
+
+let country = 'Italy';
+let continent = 'Europa';
+let population = 59.11;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+console.log(isIsland);
+
+let countryDescription =`${country} is located in ${continent}, ${population} million people live here.`;
+console.log(countryDescription);
