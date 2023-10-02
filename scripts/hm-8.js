da8314b8d3585b049ea16ef235a86638570223ac
ElenaF95 <fachira.elena@gmail.com>
2023-10-02 20:06:24 +0300

    added tasks nr.6* and nr.7* in hm-8.js

--- scripts/hm-8.js
+++ scripts/hm-8.js
@@ -37,5 +37,38 @@ const newStudent = {...student, language: 'en'};
 console.log(newStudent);
 
 //----------6*----------//
+const stringToConvert = (string) => {
+    const lowerCaseString = string.toLowerCase();
+    const charCounts = {};
+    for (const char of lowerCaseString) {
+      charCounts[char] = (charCounts[char] || 0) + 1;
+    }
+    let result = '';
+    for (const char of lowerCaseString) {
+      if (charCounts[char] > 1) {
+        result += ')';
+      } else {
+        result += '(';
+      }
+    }
+    return result;
+}
+console.log(stringToConvert('din'));
+console.log(stringToConvert('recede'));
+console.log(stringToConvert('Success'));
+console.log(stringToConvert('(( @'));
 
 //----------7*----------//
+const numbersList1 = [2, 4, 0, 100, 4, 11, 2602, 36];
+const numbersList2 = [160, 3, 1719, 19, 11, 13, -21];
+ 
+const findOutlier = (numbers) => {
+    let evens = numbers.filter((n) => n % 2 === 0);
+    let odds = numbers.filter((n) => n % 2 !== 0);
+    
+    return evens.length === 1 ? evens[0] : odds[0];
+}
+console.log(findOutlier(numbersList1));
+console.log(findOutlier(numbersList2));
+ 
+ 
