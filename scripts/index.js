35831a55e6ed8cc7eb3f58b2e76adfdb544eb298
ElenaF95 <fachira.elena@gmail.com>
2023-10-10 18:11:03 +0300

    added variables with the using colors

--- scripts/index.js
+++ scripts/index.js
@@ -17,52 +17,60 @@ document.addEventListener('DOMContentLoaded', function () {
     const textMessage = document.getElementById('message-field__form-text');
 
     form.addEventListener("submit", handleSubmit);
+    
     function handleSubmit(event) {
         event.preventDefault();
+
+        const successColor = '#004D40'
+        const succesBorderColor = '#BDBDBD'
+        const errorColor = '#B71C1C';
 
         const fullname = inputFullname.value;
         if (fullname === '' || fullname.length <= 2) {
             textFullname.textContent = 'The "Full Name" field cannot be empty and must contain a minimum of 3 letters';
-            textFullname.style.color = '#B71C1C';
-            inputFullname.style.borderColor = '#B71C1C';
+            textFullname.style.color = errorColor;
+            inputFullname.style.borderColor = errorColor;
         } else {
             textFullname.textContent = `Your fullname ${fullname} was added`;
-            textFullname.style.color = '#004D40';
-            inputFullname.style.borderColor = '#BDBDBD';
+            textFullname.style.color = successColor;
+            inputFullname.style.borderColor = succesBorderColor;
         }
 
         const email = inputEmail.value;
         if (email === '') {
             textEmail.textContent = 'Please enter your email';
-            textEmail.style.color = '#B71C1C';
-            inputEmail.style.borderColor = '#B71C1C';
+            textEmail.style.color = errorColor;
+            inputEmail.style.borderColor = errorColor;
         } else {
             textEmail.textContent = `Your email ${email} was added`;
-            textEmail.style.color = '#004D40';
-            inputEmail.style.borderColor = '#BDBDBD';
+            textEmail.style.color = successColor;
+            inputEmail.style.borderColor = succesBorderColor;
         }
+
+        const selectedItemColor = '#000000';
 
         const selectDay = inputSelectDay.value;
         if (selectDay === "select") {
             textSelectDay.textContent = 'Please select day';
-            textSelectDay.style.color = '#B71C1C';
-            inputSelectDay.style.borderColor = '#B71C1C';
+            textSelectDay.style.color = errorColor;
+            inputSelectDay.style.borderColor = errorColor; 
         } else {
             textSelectDay.textContent = 'The day was selected';
-            textSelectDay.style.color = '#004D40';
-            inputSelectDay.style.borderColor = '#BDBDBD';
-            inputSelectDay.style.textColor = '#BDBDBD'
+            textSelectDay.style.color = successColor;
+            inputSelectDay.style.borderColor = succesBorderColor;
+            inputSelectDay.style.color = selectedItemColor;
         }
 
         const selectTime = inputSelectTime.value;
         if (selectTime === "select") {
             textSelectTime.textContent = 'Please select time';
-            textSelectTime.style.color = '#B71C1C';
-            inputSelectTime.style.borderColor = '#B71C1C';
+            textSelectTime.style.color = errorColor;
+            inputSelectTime.style.borderColor = errorColor;
         } else {
             textSelectTime.textContent = 'The time was selected';
-            textSelectTime.style.color = '#004D40';
-            inputSelectTime.style.borderColor = '#BDBDBD';
+            textSelectTime.style.color = successColor;
+            inputSelectTime.style.borderColor = succesBorderColor;
+            inputSelectTime.style.color = selectedItemColor;
         }
 
         const message = inputMessage.value;
@@ -70,12 +78,12 @@ document.addEventListener('DOMContentLoaded', function () {
         const wordsCount = wordsList.length;
         if (message === '' || wordsCount <= 2) {
             textMessage.textContent = 'The "Message" field cannot be empty and must contain a minimum of 3 words';
-            textMessage.style.color = '#B71C1C';
-            inputMessage.style.borderColor = '#B71C1C';
+            textMessage.style.color = errorColor;
+            inputMessage.style.borderColor = errorColor;
         } else {
             textMessage.textContent = 'Your message was added';
-            textMessage.style.color = '#004D40';
-            inputMessage.style.borderColor = '#BDBDBD';
+            textMessage.style.color = successColor;
+            inputMessage.style.borderColor = succesBorderColor;
         }
     }
 });
